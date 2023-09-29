b5485be6f19f59ee3a44e66943d749b6a69c939d
Scot MacDonald <scotmacdonald@hotmail.com>
2023-09-29 12:03:30 +0200

    do first six questions

--- JS-basics/js-variables-and-numbers_simple-calculator/js/index.js
+++ JS-basics/js-variables-and-numbers_simple-calculator/js/index.js
@@ -14,27 +14,54 @@ const exponentButton = document.querySelector('[data-js="exponent"]');
 const moduloButton = document.querySelector('[data-js="modulo"]');
 
 addButton.addEventListener("click", () => {
+  const sum = operand1 + operand2;
+  console.log("The sum of " + operand1 + " and " + operand2 + " is: " + sum);
   // 1. Add the two operands and store the result in a variable. Log the variable's value to the console.
 });
 
 subtractButton.addEventListener("click", () => {
+  const minus = operand1 - operand2;
+  console.log(
+    "The sum of " + operand1 + " minus " + operand2 + " is: " + minus
+  );
   // 2. Subtract the two operands and store the result in a variable. Log the variable's value to the console.
 });
 
 multiplyButton.addEventListener("click", () => {
   // 3. Multiply the two operands and store the result in a variable. Log the variable's value to the console.
+  const multiply = operand1 * operand2;
+  console.log(
+    "The sum of " + operand1 + " multiplied by " + operand2 + " is: " + multiply
+  );
 });
 
 divideButton.addEventListener("click", () => {
   // 4. Divide the two operands and store the result in a variable. Log the variable's value to the console.
+  const divide = operand1 / operand2;
+  console.log(
+    "The sum of " + operand1 + " divided by " + operand2 + " is: " + divide
+  );
 });
 
 exponentButton.addEventListener("click", () => {
   // 5. Raise "operand1" to the value of "operand2" and store the result in a variable. Log the variable's value to the console.
+  const raise = operand1 ** operand2;
+  console.log(
+    "The sum of " + operand1 + " to the power of " + operand2 + " is: " + raise
+  );
 });
 
 moduloButton.addEventListener("click", () => {
   // 6. Calculate the remainder when dividing "operand1" by "operand2" and store the result in a variable. Log the variable's value to the console.
+  const raise = operand1 % operand2;
+  console.log(
+    "The remainder of " +
+      operand1 +
+      " is divided by " +
+      operand2 +
+      " is: " +
+      raise
+  );
 });
 
 /*
