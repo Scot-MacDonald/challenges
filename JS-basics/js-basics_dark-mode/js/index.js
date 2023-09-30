57a50ea9264058c3d946232db77cbe90d08e2b40
Scot MacDonald <scotmacdonald@hotmail.com>
2023-09-30 11:31:16 +0200

    complete dark mode

--- JS-basics/js-basics_dark-mode/js/index.js
+++ JS-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const dark = document.querySelector('[data-js="dark-mode-button"]');
+const light = document.querySelector('[data-js="light-mode-button"]');
+const toggle = document.querySelector('[data-js="toggle-button"]');
+
+dark.addEventListener("click", () => {
+  bodyElement.classList.add("button--light");
+  bodyElement.classList.add("button--dark");
+});
+
+light.addEventListener("click", () => {
+  bodyElement.classList.remove("button--dark");
+  bodyElement.classList.add("button--light");
+});
+
+toggle.addEventListener("click", () => {
+  bodyElement.classList.toggle("button--dark");
+  bodyElement.classList.toggle("button--light");
+});
