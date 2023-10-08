07983c6ddfac2fd7dd654a1319e122313a976ec6
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-08 18:22:46 +0200

    complete checkbox-input

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,37 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccessMessage() {
+  successMessage.setAttribute("hidden", "");
+}
+
+function showSuccessMessage() {
+  successMessage.removeAttribute("hidden");
+}
+
+// Add an event listener for the checkbox's "change" event
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError(); // Hide the error message when the checkbox is checked
+  } else {
+    showTosError(); // Show the error message when the checkbox is unchecked
+  }
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // --v-- write your code here --v--
+  if (!tosCheckbox.checked) {
+    // ! is a logical NOT operator
+    showTosError();
+    return;
+  }
 
-  // --^-- write your code here --^--
+  hideTosError();
+  showSuccessMessage();
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
 });
+
+// I could not quite get this to work, so I asked the machine for help
