b7a554d3c91297e8960dcdca7676278a5bd0aabf
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-09 20:01:07 +0200

    coded with Klaus- stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,28 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-
+  for (let i = 1; i <= 5; i++) {
+    const image = document.createElement("img");
+    // image.src("./assets/star-filled");
+    if (filledStars >= i) {
+      image.setAttribute("src", "./assets/star-filled.svg");
+      image.setAttribute("index", i);
+    } else {
+      image.setAttribute("src", "./assets/star-empty.svg");
+      image.setAttribute("index", i);
+    }
+    image.addEventListener("click", () => {
+      console.log("index", i);
+      renderStars(i);
+    });
+    starContainer.append(image);
+  }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(4);
