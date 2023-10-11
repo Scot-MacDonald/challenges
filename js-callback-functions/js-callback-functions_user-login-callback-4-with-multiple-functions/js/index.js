541cc6556291decebc4ee18c660edb7c54a70216
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-11 17:18:21 +0200

    complete task 4

--- js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -9,3 +9,25 @@ function handleUserLogin(onSuccess, onError, userName) {
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage}`);
+}
+
+function handleUserLogin(onSuccess, onError, userName) {
+  if (userName === "Jane Doe") {
+    onSuccess(userName, "admin");
+  } else {
+    onError(`Unknown user "${userName}"`);
+  }
+}
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
