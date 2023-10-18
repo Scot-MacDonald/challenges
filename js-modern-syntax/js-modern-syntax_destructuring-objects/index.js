96ab51f2fed24ce1d4f47da56e3c9c962b904528
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-18 12:06:39 +0200

    destructuring-objects

--- js-modern-syntax/js-modern-syntax_destructuring-objects/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-objects/index.js
@@ -10,6 +10,9 @@ const company = {
   location: "Berlin",
 };
 
+const { course } = company;
+console.log(course);
+
 // example: export const {value1} = myObject;
 
 // EXERCISE 2
@@ -20,6 +23,8 @@ const company = {
 //  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
 
 const user = { name: "John", years: 30 };
+const { name, years: age, isAdmin = false } = user;
+console.log(name, age, isAdmin);
 
 // EXERCISE 3
 // Extract the variables from the dog object and
@@ -31,6 +36,9 @@ const dog = {
   age: 5,
 };
 
+const { name: dogName, breed: dogBreed, age: dogAge } = dog;
+console.log(dogName, dogBreed, dogAge);
+
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
 // Store all other properties in an object called "moreInformation".
@@ -40,3 +48,6 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+
+const { lastName: personLastName, ...moreInformation } = person;
+console.log(personLastName, moreInformation);
