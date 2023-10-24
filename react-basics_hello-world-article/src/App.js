55adba6d9cc2f6d81c2fb6513790e9644ce0c61d
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-24 11:25:40 +0200

    HW article

---
+++ react-basics_hello-world-article/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css";
+import HelloWorld from "./HelloWorld";
+
+export default function App() {
+  return <HelloWorld />;
+}
---
+++ react-basics_hello-world-article/src/HelloWorld.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return HelloWorld();
+}
+
+function HelloWorld() {
+  return (
+    <article>
+      <h1>Replace me with your Component!</h1>
+      <p>look some is happening here</p>
+    </article>
+  );
+}
