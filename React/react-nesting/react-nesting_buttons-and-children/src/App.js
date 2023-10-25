23f318b82d551e575e5366327334ae8cc9232fe4
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-25 11:07:52 +0200

    bt and children

--- React/react-nesting/react-nesting_buttons-and-children/src/App.js
+++ React/react-nesting/react-nesting_buttons-and-children/src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import "./styles.css";
-
+import { Button } from "./components/Button";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>To the moon</Button>
+      <Button>To the sun</Button>
+      <Button>To nowhere</Button>
+      <Button>To somewhere</Button>
     </main>
-  );
-}
-
-function Button() {
-  return (
-    <button className="button" type="button">
-      Click me!
-    </button>
   );
 }
