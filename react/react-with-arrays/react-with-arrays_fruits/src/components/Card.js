e5827f98bd531c9274f91ef0be1945436c139c6e
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-27 11:18:09 +0200

    fruits

--- react/react-with-arrays/react-with-arrays_fruits/src/components/Card.js
+++ react/react-with-arrays/react-with-arrays_fruits/src/components/Card.js
@@ -1,5 +1,11 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ name, color, harvest }) {
+  return (
+    <p className="card" style={{ backgroundColor: color }}>
+      <li>Name: {name}</li>
+      <li>Color: {color}</li>
+      <li>When to Harvest: {harvest}</li>
+    </p>
+  );
 }
