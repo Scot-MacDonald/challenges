af6bc87314daddc138c237b5468e01e24fc2da38
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-30 13:04:34 +0100

    tags

--- react/react-state-3/react-state-3_tags/src/App.js
+++ react/react-state-3/react-state-3_tags/src/App.js
@@ -6,10 +6,20 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  const handleAddTag = (tag) => {
+    // console.log(tag);
+    const newArray = [...tags, tag];
+    setTags(newArray);
+  };
+
+  const handleDeleteTag = (clickedTag) => {
+    setTags(tags.filter((tag) => tag !== clickedTag));
+  };
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
